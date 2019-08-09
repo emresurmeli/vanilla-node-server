2c6ebf9c8cae31f95bc4708c401b85e9a99d8d16
Emre Surmeli <s.emre.s.8@gmail.com>
2019-08-09 13:20:26 -0400

    Update routes and handle a 404 case

--- httpServer.js
+++ httpServer.js
@@ -1,25 +1,37 @@
 const http = require('http')
 const moment = require('moment')
 
-const server = http.createServer((request, response) => {
-  if (request.url === '/time') {
-    const dateTime = moment().format("MMM Do YY")
-
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    response.write(JSON.stringify({time: dateTime}))
-    response.end()
+const server = http.createServer((req, res) => {
+  // Route for /
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.writeHead(200, {'Ya': 'Trick'}) // We can send whatever header we want
+    res.end()
   }
+  // Route for /time
+  else if (req.url === '/time') {
+    const dateTime = moment().format('MMMM Do YYYY, h:mm:ss a')
 
-  if (request.url === '/greet') {
-    if (request.method === 'POST') {
-      request.on('data', async (data) => {
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.write(JSON.stringify({time: dateTime}))
+    res.end()
+  }
+  // Route for /greet
+  else if (req.url === '/greet') {
+    if (req.method === 'POST') {
+      req.on('data', async (data) => {
         const body = JSON.parse(data.toString())
 
-        response.writeHead(200, {'Content-Type': 'application/json'})
-        response.write(JSON.stringify({ message: `Hello ${body.name}`}))
-        response.end()
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.write(JSON.stringify({ message: `Hello ${body.name}`}))
+        res.end()
       })
     }
+  }
+  else {
+    res.writeHead(404, {'Content-Type': 'application/json'})
+    res.write(JSON.stringify({ 404: `Page not found`}))
+    res.end()
   }
 }).listen(3000, () => {
   console.log('Server running on port 3000')
